d2d5caf25b6a2a7e870676df55f84587842bd2b8
khaled-al-hamwie <khaledalhamwiedevelopmacccount@gmail.com>
2023-11-03 21:15:50 +0300

    adding the hero section image

--- src/components/main/hero/hero-images/hero-images.jsx
+++ src/components/main/hero/hero-images/hero-images.jsx
@@ -1,3 +1,10 @@
+import hero from "../../../../images/hero.svg";
+import "./hero-images.css";
+
 export default function HeroImages() {
-	return <div className="hero-images">hu</div>;
+	return (
+		<div className="hero-images-container">
+			<img src={hero} alt="hero" />
+		</div>
+	);
 }
