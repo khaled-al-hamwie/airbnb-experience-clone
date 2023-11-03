63536da9372cd1ba51975e0e035e222173cee1a3
khaled-al-hamwie <khaledalhamwiedevelopmacccount@gmail.com>
2023-11-03 21:22:07 +0300

    adding the hero title

--- src/components/main/hero/hero-title/hero-title.jsx
+++ src/components/main/hero/hero-title/hero-title.jsx
@@ -1,3 +1,12 @@
+import "./hero-title.css";
 export default function HeroTitle() {
-	return <div className="hero-title">titl</div>;
+	return (
+		<div className="hero-title">
+			<h1>Online Experiences</h1>
+			<p>
+				Join unique interactive activities led by one-of-a-kind
+				hosts—all without leaving home.
+			</p>
+		</div>
+	);
 }
